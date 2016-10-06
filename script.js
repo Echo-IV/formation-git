fbd64eceaacb90c78cb1ef3a6740d9872660d0ac
Alexis Phuong <alexis.hvp@gmail.com>
2016-10-06 10:35:55 +0200

    refactor

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-function myMove() {
+var my_move = function() {
 
     var elem = document.getElementById("myAnimation");
+
     var pos = 0;
+
     var interval = setInterval(frame, 10);
 
     function frame() {
         if (pos == 350) {
+
             clearInterval(interval);
         } else {
+
             pos++;
+
             elem.style.top = pos + 'px';
         }
     }
